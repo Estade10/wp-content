a1da4f4c3766afecdadca8a6139725ea15fa654c
Esther <eadetitun@gmail.com>
2023-10-04 13:25:54 +0200

    init

---
+++ plugins/bit-form/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '2.5.2.x-dev',
+        'version' => '2.5.2.9999999-dev',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '6f58e426cb44b2898f059071511ded9eea269514',
+        'name' => 'bitcode/bitform',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'bitcode/bitform' => array(
+            'pretty_version' => '2.5.2.x-dev',
+            'version' => '2.5.2.9999999-dev',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '6f58e426cb44b2898f059071511ded9eea269514',
+            'dev_requirement' => false,
+        ),
+    ),
+);
